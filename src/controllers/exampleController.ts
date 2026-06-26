7aaab6fc04e750a77c5be5779384598bc60135aa
simago02 <simoneago44@gmail.com>
2026-06-26 16:02:04 +0200

    Deleted example route and controller

--- src/controllers/exampleController.ts
+++
@@ -1,8 +0,0 @@
-import type { Request, Response } from "express"
-
-export class ExampleController {
-  public cheers = async (req: Request, res: Response): Promise<void> => {
-    const name: string = req.body?.name ?? 'Simone'
-    res.status(200).send("Hello " + name)
-  }
-}
--- src/routes/exampleRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { ExampleController } from "../controllers/exampleController.ts";
-
-const router = Router();
-const controller = new ExampleController()
-
-router.get("/cheers", controller.cheers)
-
-export default router
