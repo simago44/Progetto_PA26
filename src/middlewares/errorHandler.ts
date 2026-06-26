cd287f0781199b89bf0a1aeb9617548d03eeca2a
simago02 <simoneago44@gmail.com>
2026-06-26 16:41:33 +0200

    changed errorHandler to handle generic Error and not only AppError

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,19 +1,24 @@
 // middleware/errorHandler.ts
 import type { Request, Response, NextFunction } from "express";
-import { AppError } from "../factory/errorFactory.ts";
+import { AppError, createError, ErrorEnum } from "../factory/errorFactory.ts";
+import logger from "./logger.ts";
 
 /**
  * Global Express error handler middleware.
- * Responds with the HTTP status and message from the `AppError`.
- * 
- * @see AppError
+ * Responds with the HTTP status and message from the `Error`.
  */
-export function errorHandler(err: AppError, req: Request, res: Response, next: NextFunction): void {
-  const responseObject: {name: string, error: string, detail?: string} = {
-    name: err.name,
-    error: err.message
+export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): void {
+  const error = err instanceof AppError ? err : createError(ErrorEnum.InternalServer)
+
+  if (!(err instanceof AppError)) {
+    logger.error("Unhandled error:", err); // logga l'errore originale
   }
-  if(err.detail) responseObject.detail = err.detail;
   
-  res.status(err.status).json(responseObject)
+  const responseObject: { name: string, error: string, detail?: string } = {
+    name: error.name,
+    error: error.message
+  }
+  if ( error.detail) responseObject.detail = error.detail;
+
+  res.status(error.status).json(responseObject)
 }
