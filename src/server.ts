2d9ace917aaeda45fe9c10687371cf69b9b6ce32
simago02 <simoneago44@gmail.com>
2026-06-27 09:37:49 +0200

    Added postman route for creating auction
    Auction parameters can be modified in the body

--- src/server.ts
+++ src/server.ts
@@ -3,36 +3,36 @@
  * Registers middleware, routes, and starts the HTTP server.
  */
 
-import express from 'express';
+import express from "express";
 import authRoutes from "./routes/authRoutes.ts";
-import auctionRoutes from "./routes/auctionRoutes.ts"
-import { errorHandler } from './middlewares/errorHandler.ts';
-import logger from './middlewares/logger.ts';
-import env from './config.ts';
-import { healthCheck } from './utils/healthUtils.ts';
-import { initDb } from './services/initDb.ts';
+import auctionRoutes from "./routes/auctionRoutes.ts";
+import { errorHandler } from "./middlewares/errorHandler.ts";
+import logger from "./middlewares/logger.ts";
+import env from "./config.ts";
+import { healthCheck } from "./utils/healthUtils.ts";
+import { initDb } from "./services/initDb.ts";
 
-import './services/BullMQ.ts'
+import "./services/BullMQ.ts";
 
 await initDb();
 
 // Initialize Express app
 const app = express();
-const PORT = env.NODE_PORT
+const PORT = env.NODE_PORT;
 
 // Middleware for parsing json bodies
 app.use(express.json());
 
-app.get('/health', healthCheck);
+app.get("/health", healthCheck);
 
 // Start server
 app.listen(PORT, () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
 });
 
-app.use("/auth", authRoutes)
-app.use("/auction", auctionRoutes )
+app.use("/auth", authRoutes);
+app.use("/auctions", auctionRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 export { app };
