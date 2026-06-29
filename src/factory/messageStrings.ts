816117f406687eae484c8da01a09710700b319af
simago02 <simoneago44@gmail.com>
2026-06-29 16:26:09 +0200

    Fixed position of message string

--- src/factory/messageStrings.ts
+++ src/factory/messageStrings.ts
@@ -26,9 +26,9 @@ export const validationError_message: string = "ERROR - Validation Error";
 export const databaseError_message: string = "ERROR - Database Error";
 export const invalidAuctionStatus_message: string =
   "ERROR - Invalid auction status";
+export const wrongUsernameOrPassword_message: string =
+  "Wrong username or password";
 
 // Success
 export const userCreatedSuccessfully_message: string =
   "User created successfully";
-export const wrongUsernameOrPassword_message: string =
-  "Wrong username or password";
