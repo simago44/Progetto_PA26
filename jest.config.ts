5f3aa5b6746f939858253b1cfe04cd8ae78fff24
simago02 <simoneago44@gmail.com>
2026-07-07 13:02:17 +0200

    Fixed tests configuration

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,14 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", {
       useESM: true,
       tsconfig: "tsconfig.spec.json",
     }],
   },
-  moduleNameMapper: {
-    // strip .ts extension from imports so Jest can resolve them
-    "^(\\.{1,2}/.*)\\.ts$": "$1",
-  },
-}
+};
 
-export default jestConfig
+export default jestConfig;
